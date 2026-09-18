0c869b2850825ccd7d3caa4a755355e26ad4d7d7
Sanket <sanket@192.168.1.238>
2026-09-18 18:24:49 +0100

    Added basic prisma setup

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -5,4 +5,5 @@ dotenv.config({ path: path.join(__dirname, "..", "..", ".env") });
 
 export const env = {
   API_PORT: Number(process.env.API_PORT) || 3000,
+  DATABASE_URL: process.env.DATABASE_URL ?? "",
 } as const;
